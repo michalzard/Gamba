a552c020a1da7180ae566804f5701cd8baa830de
MichalPlatko <miso.platko@gmail.com>
2021-09-04 17:19:16 +0200

    grabs sessionID from localstorage and passes it to react components so they correctly visually update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import {CssBaseline} from "@material-ui/core"
 import "./style/App.scss";
 import Content from "./components/Content";
 import Chat from "./components/Chat";
+import { useState,useEffect } from "react";
+import axios from "axios";
 
 function App() {
+
+    const [sessionID,setSessionID]=useState(null);
+
+     /**
+     * JUST TO TEST
+     */
+      useEffect(() => {
+        if(localStorage.getItem("sessionID")){
+        setSessionID(localStorage.getItem("sessionID"))
+        axios.get(`http://localhost:3001/session?lastSession=${localStorage.getItem("sessionID")}`).then(data=>{
+        const {message}=data.data;
+        if(message.includes("Session wasn't found")){setSessionID(null);localStorage.removeItem("sessionID");}
+        });
+      }
+    }, []);
+
   return (
     <div className="App">
     <CssBaseline/>
     <Chat/>
-    <Content/>
+    <Content sessionID={sessionID} setSessionID={setSessionID} />
     </div>
   );
 }
